20090ad95b2535968f272fec8dda011a2654a4c8
Euclides Costa <114588979+Euclides-Costa@users.noreply.github.com>
2023-12-31 11:25:02 -0300

    Testando

---
+++ Aulas/aula02.js
@@ -0,0 +1,9 @@
+const x = 2; //é uma constante
+let y = 9; //posso mudar o valor da variável
+
+if (x === 2){
+    //x = 8; //erro
+    y = 2;
+}
+
+console.log("Resultado:", y); //printar
---
+++ Aulas/aula03.js
@@ -0,0 +1,11 @@
+const idade = 18;
+const nome = "Netinho";
+
+if (idade === 18 && nome === "Netinho"){
+    console.log("Pode entrar!!");
+}
+else{
+    console.log("Não pode entrar!!")
+}
+// == faz conversão
+// === não faz conversão
