5c8c624784988c5ee7072ac3823f73259e8483b6
Euclides Costa <114588979+Euclides-Costa@users.noreply.github.com>
2024-01-15 11:16:43 -0300

    Conversão string

---
+++ aula08.js
@@ -0,0 +1,5 @@
+const salario = "103";
+const salarioNumero = Number(salario); //passando em número
+
+console.log("Salário: ", salario);
+console.log("Salário: ", salarioNumero.toFixed(2)); //passa para string e deixa o valor fixo com 2 casas decimais
